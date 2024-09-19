3f11d67485e0459c532a8d1c11439cf1b0bf02a4
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-19 23:33:35 +0530

    added actor struct

--- internal/model/event.go
+++ internal/model/event.go
@@ -1,11 +1,16 @@
 package model
 
 type Actor struct {
-	ID string `json:"id"`
+	ID           string `json:"id"`
+	Login        string `json:"login"`
+	DisplayLogin string `json:"display_login"`
+	GravatarID   string `json:"gravatar_id"`
+	URL          string `json:"url"`
+	AvatarURL    string `json:"avatar_url"`
 }
 
 type Event struct {
-	ID    int64  `json:"id`
+	ID    int64  `json:"id"`
 	Type  string `json:"type"`
 	Actor Actor  `json:"actor"`
 }
