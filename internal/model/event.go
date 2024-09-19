337a2b86f9c734054dfd3b06e9e3c134477514c9
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-19 23:35:04 +0530

    added repo struct model

--- internal/model/event.go
+++ internal/model/event.go
@@ -9,8 +9,15 @@ type Actor struct {
 	AvatarURL    string `json:"avatar_url"`
 }
 
+type Repo struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
 type Event struct {
 	ID    int64  `json:"id"`
 	Type  string `json:"type"`
 	Actor Actor  `json:"actor"`
+	Repo  Repo   `json:"repo"`
 }
