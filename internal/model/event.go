6b741f933f46763f675f78d59c3ed8ef6c21704d
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-24 17:02:04 +0530

    added some changes in the struct model

--- internal/model/event.go
+++ internal/model/event.go
@@ -1,7 +1,17 @@
 package model
 
+type Event struct {
+	ID        string  `json:"id"`
+	Type      string  `json:"type"`
+	Actor     Actor   `json:"actor"`
+	Repo      Repo    `json:"repo"`
+	Payload   Payload `json:"payload"`
+	Public    bool    `json:"public"`
+	CreatedAt string  `json:"created_at"`
+}
+
 type Actor struct {
-	ID           int64  `json:"id"`
+	ID           int    `json:"id"`
 	Login        string `json:"login"`
 	DisplayLogin string `json:"display_login"`
 	GravatarID   string `json:"gravatar_id"`
@@ -10,14 +20,20 @@ type Actor struct {
 }
 
 type Repo struct {
-	ID   int64  `json:"id"`
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
 
-type Author struct {
-	Email string `json:"email"`
-	Name  string `json:"name"`
+type Payload struct {
+	RepositoryID int      `json:"repository_id"`
+	PushID       int64    `json:"push_id"`
+	Size         int      `json:"size"`
+	DistinctSize int      `json:"distinct_size"`
+	Ref          string   `json:"ref"`
+	Head         string   `json:"head"`
+	Before       string   `json:"before"`
+	Commits      []Commit `json:"commits"`
 }
 
 type Commit struct {
@@ -28,23 +44,7 @@ type Commit struct {
 	URL      string `json:"url"`
 }
 
-type Payload struct {
-	RepositoryID int64    `json:"repository_id"`
-	PushID       int64    `json:"push_id"`
-	Size         int8     `json:"size"`
-	DistinctSize int8     `json:"distinct_size"`
-	Ref          string   `json:"ref"`
-	Head         string   `json:"head"`
-	Before       string   `json:"before"`
-	Commits      []Commit `json:"commits"`
-}
-
-type Event struct {
-	ID        int64   `json:"id"`
-	Type      string  `json:"type"`
-	Actor     Actor   `json:"actor"`
-	Repo      Repo    `json:"repo"`
-	Payload   Payload `json:"payload"`
-	Public    bool    `json:"public"`
-	CreatedAt string  `json:"created_at"`
+type Author struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
 }
