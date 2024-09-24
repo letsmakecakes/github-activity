df66ba531f4015cfd7ca786dfe0e888234fcce42
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-24 17:02:14 +0530

    added main program

--- cmd/github-activity/main.go
+++ cmd/github-activity/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/letsmakecakes/github-activity/pkg/cli"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Panic("expected a username")
+	}
+
+	cli.HandleCommand(args)
+}
